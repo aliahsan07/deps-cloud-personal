3bd8cde509635a402645843a18f8eb4963ec37dd
Ali Ahsan <aliahsan07@outlook.com>
2020-01-24 03:51:38 -0600

    WIP, minimal structure of bitbucket code setup

--- repo-metadata/main.go
+++ repo-metadata/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
-
-	"credentials"
-
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
+	"github.com/davidji99/bitbucket-go/bitbucket"
 )
+
+// state (active|archived|unknown)
+// last modified
+// last commit
+// number of stars
+// number of forks
+// number of watches
 
 type repository struct {
 	ID              int64  `json:"id"`
@@ -18,48 +20,66 @@ type repository struct {
 	OpenIssues      int    `json:"open_issues_count"`
 	StargazersCount int    `json:"stargazers_count"`
 	WatchersCount   int    `json:"watchers_count"`
+	IsPrivate       bool   `json:"private"`
 }
 
 func main() {
 
 	// Get repos for authenticated user
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: credentials.GithubAccessToken},
-	)
 
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
+	client := bitbucket.NewBasicAuth("aliahsan07@outlook.com", "------")
 
-	repos, _, err := client.Repositories.List(ctx, "", nil)
+	result, _, error := client.Repositories.List("aliahsan07")
 
-	if err != nil {
-		fmt.Println(err)
+	if error != nil {
+		fmt.Println("error encountered")
 	}
 
-	refinedRepos := make([]*repository, 0)
+	repos := result.Values
 
-	for _, t := range repos {
-		var repoDetails *repository = new(repository)
+	for _, repo := range repos {
+		fmt.Println(repo.GetTitle())
+		fmt.Println(repo.GetCreatedOn())
+		fmt.Println(repo.GetEditedOn())
+		fmt.Println(repo.GetState())
+		fmt.Println(repo.GetUpdatedOn())
+		fmt.Println(repo.GetWatches())
+		fmt.Println(repo.GetRepository().GetSlug())
 
-		repoDetails.ID = t.GetID()
-		repoDetails.Name = t.GetName()
-		repoDetails.FullName = t.GetFullName()
-		repoDetails.ForksCount = t.GetForksCount()
-		repoDetails.OpenIssues = t.GetOpenIssuesCount()
-		repoDetails.StargazersCount = t.GetStargazersCount()
-		repoDetails.WatchersCount = t.GetWatchersCount()
-
-		refinedRepos = append(refinedRepos, repoDetails)
 	}
 
-	for _, repo := range refinedRepos {
-		fmt.Println("Name of repo: " + repo.FullName)
-		fmt.Println("Open issues: ", repo.OpenIssues)
-		fmt.Println("Count of stargazers: ", repo.StargazersCount)
-		fmt.Println("Number of forks: ", repo.ForksCount)
-		fmt.Println("Number of watchers: ", repo.WatchersCount)
-		fmt.Println("===============================")
-	}
+	// repos, _, err := client.Repositories.List(ctx, "", nil)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// refinedRepos := make([]*repository, 0)
+
+	// for _, t := range repos {
+	// 	var repoDetails *repository = new(repository)
+
+	// 	repoDetails.ID = t.GetID()
+	// 	repoDetails.Name = t.GetName()
+	// 	repoDetails.FullName = t.GetFullName()
+	// 	repoDetails.ForksCount = t.GetForksCount()
+	// 	repoDetails.OpenIssues = t.GetOpenIssuesCount()
+	// 	repoDetails.StargazersCount = t.GetStargazersCount()
+	// 	repoDetails.WatchersCount = t.GetWatchersCount()
+	// 	repoDetails.IsPrivate = t.GetPrivate()
+	// 	t.GetArchived()
+
+	// 	refinedRepos = append(refinedRepos, repoDetails)
+	// }
+
+	// for _, repo := range refinedRepos {
+	// 	fmt.Println("Name of repo: " + repo.FullName)
+	// 	fmt.Println("Open issues: ", repo.OpenIssues)
+	// 	fmt.Println("Count of stargazers: ", repo.StargazersCount)
+	// 	fmt.Println("Number of forks: ", repo.ForksCount)
+	// 	fmt.Println("Number of watchers: ", repo.WatchersCount)
+	// 	fmt.Println("Is Private: ", repo.IsPrivate)
+	// 	fmt.Println("===============================")
+	// }
 
 }
